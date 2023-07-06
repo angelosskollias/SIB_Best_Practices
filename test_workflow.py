a74d01d0612bd84ab2433aedb8082ea3deecd575
Angelos Kollias <angelos.kollias@unil.ch>
2023-07-06 11:41:03 +0200

    unit_test_Dataframe

--- test_workflow.py
+++ test_workflow.py
@@ -1,1 +1,11 @@
 from unittest import TestCase
+from workflow import run_workflow
+import pandas as pd
+
+class RunWorkflowTest(TestCase):
+    
+    def test_output_dataframe(self):
+        dataframe = pd.DataFrame()
+        df_out = run_workflow(dataframe)
+        self.assertIsInstance(df_out, pd.DataFrame)
+        
